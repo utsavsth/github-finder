8e18df2fd5c71fe6f3a64c6d4868b75c5526ddcf
Utsav Shrestha <utsav@norbrik.com>
2022-03-22 21:52:03 +1100

    daisyui and hook navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { 
+  BrowserRouter as  Router, 
+  Route 
+} 
+from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className='text-xl'>Hello World</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
