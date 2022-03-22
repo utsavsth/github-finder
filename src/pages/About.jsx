fe9bbbf543b4b420f480a3b0d9e436dad85cfa06
Utsav Shrestha <utsav@norbrik.com>
2022-03-22 22:28:45 +1100

    create pages and setup routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+function About() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-black'> 1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Developed By:
+        <a className='text-black' href='/'> Utsav Shrestha</a>
+      </p>
+    </div>
+  );
+}
+
+export default  About
