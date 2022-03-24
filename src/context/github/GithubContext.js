415089ff427e033665dbc96b0a8fbe62d7eee00e
Utsav Shrestha <utsav@norbrik.com>
2022-03-24 22:42:01 +1100

    code refactor: move getuser fn from context to separate Action file and calling dispatch from the component now.

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -16,28 +16,6 @@ export const GithubProvider = ({children}) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text
-    })
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        //Authorization: `token ${GITHUB_TOKEN}`
-      }
-    });
-    const {items} = await response.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items
-    })
-    //console.log(data);
-  }
-
   // Get single user
   const getUser = async (login) => {
     setLoading();
@@ -114,11 +92,8 @@ export const GithubProvider = ({children}) => {
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
   return <GithubContext.Provider value={{
-    users: state.users,
-    user: state.user,
-    repos: state.repos,
-    loading: state.loading,
-    searchUsers,
+    ...state,
+    dispatch,
     fetchUsers,
     getUser,
     getUserRepos,
